3483a29335ebf0d8fb4c225cce23435cdc8e60c3
Kaush Jonna <kaush.jonna@gmail.com>
2019-01-21 15:51:48 +0000

    completed 'howManyHundreds' function evaluation

--- exercises.js
+++ exercises.js
@@ -1,4 +1,8 @@
-function ageCalculator (name, yearOfBirth, currentYear) {
-  var age = currentYear - yearOfBirth;
-  return name+ " is " +age+ " years old.";
-}
+// function ageCalculator (name, yearOfBirth, currentYear) {
+//   var age = currentYear - yearOfBirth;
+//   return name+ " is " +age+ " years old.";
+// }
+
+// function howManyHundreds (num) {
+//   return Math.trunc(num/100);
+// }
